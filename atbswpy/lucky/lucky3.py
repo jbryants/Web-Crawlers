64781f57e1b017bdd3f4e4a2c9c0623730241c84
jbryants <40534371+jbryants@users.noreply.github.com>
2020-03-23 18:15:06 +0530

    Add files via upload

---
+++ atbswpy/lucky/lucky3.py
@@ -0,0 +1,16 @@
+#! python3
+# Open several Google search results.
+
+import requests, sys, webbrowser, bs4
+
+print('Googling...') # display text while downloading the Google page
+res = requests.get('http://google.com/search?q=' + ' '.join(sys.argv[1:]))
+res.raise_for_status()
+
+soup = bs4.BeautifulSoup(res.text, features='lxml')
+linkElems = soup.select('.r a')
+numOpen = min(5, len(linkElems))
+for i in range(numOpen):
+    urlToOpen = 'http://google.com' + linkElems[i].get('href')
+    print('Opening', urlToOpen)
+    webbrowser.open(urlToOpen)
---
+++ atbswpy/lucky/lucky5.py
@@ -0,0 +1,26 @@
+#! python3
+# lucky.py - Opens several Google search results.
+  
+import webbrowser
+import bs4
+import requests
+import sys
+ 
+def my_func(search_str) :
+    print('Googling...') # display text while downloading the Google page
+    res = requests.get('http://google.com/search?source=' + ' '.join(search_str))
+    print(res.raise_for_status())
+  
+    # Retreive top search result links.
+    soup = bs4.BeautifulSoup(res.text, "html.parser")
+  
+    # Open a browser tab for each result
+    linkElems = soup.select('.r a')
+    numOpen = min(5, len(linkElems))
+    for i in range(numOpen):
+        webbrowser.open('http://google.com' + linkElems[i].get('href'))
+ 
+def test_it():
+    my_func(sys.argv[1:])
+
+test_it()
